a275436655973e5b8dbc238681960463790509e8
eddie <eddie.lau.life@gmail.com>
2026-07-09 07:59:24 +0800

    Site updated: 2026-07-09 07:59:24

---
+++ sw.js
@@ -0,0 +1,48 @@
+importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
+workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/' });
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (names) {
+      var validSets = ["is-sw-acbad5","is-html-acbad5","is-jsdelivr-acbad5","is-gtm-acbad5","is-gravatar-acbad5","is-theme-acbad5","is-json-acbad5"];
+      return Promise.all(
+        names
+          .filter(function (name) { return !~validSets.indexOf(name); })
+          .map(function (name) {
+            indexedDB && indexedDB.deleteDatabase(name);
+            return caches.delete(name);
+          })
+      ).then(function() { self.skipWaiting() });
+    })
+  );
+});
+
+workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
+  cacheName: 'is-sw-acbad5',
+}));
+workbox.routing.registerRoute(new RegExp('https://cdn\\.jsdelivr\\.net'), workbox.strategies.staleWhileRevalidate({
+  cacheName: 'is-jsdelivr-acbad5',
+  plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
+}));
+workbox.routing.registerRoute(new RegExp('https://www\\.googletagmanager\\.com\?id=.*'), workbox.strategies.staleWhileRevalidate({
+  cacheName: 'is-gtm-acbad5',
+  plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
+}));
+workbox.routing.registerRoute(new RegExp('https://www\\.gravatar\\.com'), workbox.strategies.staleWhileRevalidate({
+  cacheName: 'is-gravatar-acbad5',
+  plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
+}));
+workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)$'), workbox.strategies.staleWhileRevalidate({
+  cacheName: 'is-theme-acbad5',
+  plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
+}));
+workbox.routing.registerRoute(new RegExp('/.*\\.json'), workbox.strategies.cacheFirst({
+  cacheName: 'is-json-acbad5',
+  plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
+}));
+
+workbox.routing.registerRoute(new RegExp('/.*(:?/[^\\.]*/?)$'), function(context) {
+  var url = context.url.pathname;
+  if (!url.endsWith('/')) url += '/';
+  return fetch(url);
+});
